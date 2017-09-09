8ac738de37212ad0d7a09fbc13cd385792247e38
VLER <ycysvler@hotmail.com>
2017-09-10 00:10:50 +0800

    * MD,忘了一个接口，按分类删除； # 苏享茂死了，程序员兄弟们，远离心机婊；远离相亲网站；

--- server/mongo/entschemas.js
+++ server/mongo/entschemas.js
@@ -15,15 +15,15 @@ module.exports = class Schemas{
             type: {type: String,index: true},       // 业务类型
             colour:{type: Number,index: true},      // 业务类型
             state:{type: Number,index: true},       // 状态 0:新图，1:正在计算特征，2：计算特征成功，-1：计算特征失败
-            source:Buffer,
-            shape_cut:Buffer,
-            color_cut:Buffer,
-            color_feature:Buffer,
-            lbp_feature:Buffer,
-            shape_feature:Buffer,
-            deep_feature:Buffer,
-            extend:String,                          // 扩展字段，放个大字符串
-            createtime:Date                         // 创建时间
+            source:Buffer,                          //  原始i图像
+            shape_cut:Buffer,                       //  形状归一图
+            color_cut:Buffer,                       //  颜色归一图
+            color_feature:Buffer,                   //  颜色特征
+            lbp_feature:Buffer,                     //  纹理特征
+            shape_feature:Buffer,                   //  形状特征
+            deep_feature:Buffer,                    //  深度学习特征
+            extend:String,                          //  扩展字段，放个大字符串
+            createtime:Date                         //  创建时间
         }); 
 
         this.Image = conn.model('Image', this.imageSchema);
