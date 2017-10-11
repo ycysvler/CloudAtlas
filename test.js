d6980f613711572c06f13d36bfe839634f3559c7
zhanghongqing <ycysvler@hotmail.com>
2017-10-11 18:03:04 +0800

    +

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
-var ffi = require('ffi');
-var path = require('path');
+var Redis = require('ioredis');
+var rediscfg = require('./server/config/redis');
+var redis = new Redis(rediscfg);
+var pub = new Redis(rediscfg);
 
- var iopath = path.join(__dirname, '/dll/patentimagematch.dll');
+redis.subscribe('news', 'music','RebuildIndex', function (err, count) {
+    // Now we are subscribed to both the 'news' and 'music' channels.
+    // `count` represents the number of channels we are currently subscribed to.
 
- console.log('iopath:', iopath);
+    pub.publish('news', 'Hello world!');
+    pub.publish('music', 'Hello again!');
+});
 
-var d = ffi.Library(iopath,{'DllJudgeImageisColor':['int32',['string']]});
-
-var colour = d.DllJudgeImageisColor("d:\\image.jpg");
-
-console.log('colour:', colour);
+redis.on('message', function (channel, message) {
+    // Receive message Hello world! from channel news
+    // Receive message Hello again! from channel music
+    console.log('Receive message %s from channel %s', message, channel);
+});
